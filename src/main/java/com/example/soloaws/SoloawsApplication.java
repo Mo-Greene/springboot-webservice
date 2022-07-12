074724b9c15a8f3b31c2238a1c660ec05692bf8e
Mo-Greene <13blueboy13@daum.net>
2022-07-12 15:45:29 +0900

    Initial commit

---
+++ src/main/java/com/example/soloaws/SoloawsApplication.java
@@ -0,0 +1,13 @@
+package com.example.soloaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloawsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoloawsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/soloaws/SoloawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soloaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloawsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
